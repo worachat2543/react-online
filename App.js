02f399259db82034e68eb36cbcab70d1e76580b6
worachat2543 <94208452+worachat2543@users.noreply.github.com>
2021-11-20 12:03:17 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Logo from './components/Logo';
+
+function App() {
+  return (
+    <div>
+      <Logo />
+     <Header />
+     <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
